4767ffe0ab22bd40f698d799a6acd5b91bec5f66
dimitri23559 <63942553+dimitri23559@users.noreply.github.com>
2022-06-02 13:47:35 +0700

    final!!!!!!!

--- blog/resources/views/dashboard/posts/index.blade.php
+++ blog/resources/views/dashboard/posts/index.blade.php
@@ -33,10 +33,15 @@
                 <td>
                     <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span
                         data-feather="eye"></span></a>
-                <a href="#" class="badge bg-warning"><span
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span
                         data-feather="edit"></span></a>
-                <a href="#" class="badge bg-danger"><span
-                    data-feather="trash"></span></a>
+                        <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+                          @method('delete')
+                          @csrf
+                        <button class="badge bg-danger border-0" onclick="return confirm('are you sure?')"><span
+                          data-feather="trash"></button>
+                        </form>
+              
 
                 </td>
               </tr>
